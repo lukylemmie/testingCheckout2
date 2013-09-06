3b8dddd4057e1c64532d4d169ac3fb40e4ad06f6
lukylemmie <lemmie6@gmail.com>
2013-09-06 18:28:55 +1000

    hello2

---
+++ src/main/java/HelloNow.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Andrew2012
+ * Date: 6/09/13
+ * Time: 6:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloNow {
+    public static void main(String[] args){
+        System.out.println("HELLO AGAIN!");
+    }
+}
